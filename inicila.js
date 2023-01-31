d2bd6f6b9a0fe6e4b179faa6b0b93191fddee8e5
Nicolas Tworeck <nico.tworeck@gmail.com>
2023-01-31 13:20:12 -0300

    commit

---
+++ inicila.js
@@ -0,0 +1,9 @@
+function entrar(){
+    window.location.href="login/login.html";
+}
+function insta(){
+    window.location.href="https://www.instagram.com";
+}
+function face(){
+    window.location.href="https://pt-br.facebook.com";
+}
